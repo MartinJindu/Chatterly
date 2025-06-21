f06ed5a6d80a9819d9960539563fd6cf301aa930
MartinJindu <martinchijindu@gmail.com>
2025-06-21 03:15:33 +0100

    fix: login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,13 @@
-"use client";
 import AuthLayout from "@/components/auth/AuthLayout";
 import LoginForm from "@/components/auth/LoginForm";
+import { Suspense } from "react";
 
 export default function LoginPage() {
   return (
     <AuthLayout heading="Connect. Communicate. Collaborate.">
-      <LoginForm />
+      <Suspense fallback={<div>Loading login form...</div>}>
+        <LoginForm />
+      </Suspense>
     </AuthLayout>
   );
 }
